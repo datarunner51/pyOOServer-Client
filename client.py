9052a3e2025610b96a849f8d6207d6b62ed13cf2
datarunner51 <65912422+datarunner51@users.noreply.github.com>
2020-05-31 15:28:16 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
+import socket
+
 class client:
     def __init__(self, IP = "", PORT = ""):
         self.ip = IP
         self.port = PORT
-        self.BS = 4096
         self.socket = socket.socket()
     
     def bind(self,IP, PORT):
@@ -17,35 +18,6 @@ class client:
             data = conn.recv(1024)
         print("Received: {}".format(str(repr(data))))
         
-
-        
-    def sendfile(self, filename):
-        conn = self.connect()
-        filesize = os.path.getsize(filename)
-        SEPARATOR = "<SEPARATOR>"
-        
-        #Send information for the file
-        conn.send(f"{filename}{SEPARATOR}{filesize}".encode())
-        
-        
-        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        
-        #Openfile in binary
-        with open(filename, "rb") as f:
-            for _ in progress:
-                # read the bytes from the file
-                bytes_read = f.read(self.BS)
-                if not bytes_read:
-                    # file to bytes is done
-                    break
-                # we use sendall to assure transimission in 
-                # busy networks
-                conn.sendall(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))
-        # close the socket when complete
-        conn.close()
-        print("Complete")
         
         
         
@@ -60,9 +32,3 @@ class client:
             pass
         print("Connection Successful")
         return socketInstance
-        
-
-        
-        
-        
-    
